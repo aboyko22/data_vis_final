f29cf66374aa66ba901a8cdb83906d60f413185e
aboyko22 <alexboyko2026@u.northwestern.edu>
2024-11-16 18:21:11 -0600

    Initial Project Structure

---
+++ scripts/00_Data_Cleaning.R
@@ -0,0 +1,9 @@
+# load packages ----
+library(tidyverse)
+library(cfbfastR)
+
+# load data ----
+load("data/raw_college.rda")
+load("data/raw_northwestern_2023.rda")
+load("data/northwestern_players.rda")
+load("data/team_information.rda")
