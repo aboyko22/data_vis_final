462bb9ef0574210febe627f1a9da1ccb287a6ed2
aboyko22 <alexboyko2026@u.northwestern.edu>
2024-11-26 00:41:36 -0600

    Initial Graphic Ideas

--- scripts/01_Graphic_Creation.R
+++ scripts/01_Graphic_Creation.R
@@ -7,17 +7,56 @@ library(maps)
 
 # load data ----
 load("data/team_data.rda")
+load("data/player_data.rda")
+
+team_data <- team_data %>%
+  mutate(
+    temp = if_else(color == "#ffffff", alt_color, color),
+    alt_color = if_else(color == "#ffffff", color, alt_color),
+    color = temp) %>%
+  select(-temp)
+
+# top team bar chart ----
+team_data %>%
+  slice_max(order_by = `Total Yards`, n = 10) %>%
+  ggplot(aes(x = reorder(school, -`Total Yards`), y = `Total Yards`)) +
+  geom_col(aes(fill = color, color = alt_color), alpha = 0.85) +
+  geom_from_path(aes(path = logo), height = 0.1) +
+  scale_fill_identity() +
+  scale_color_identity() +
+  labs(x = NULL)
+
+# team scatter plot ----
+team_data %>%
+  ggplot(aes(x = `Rush Attempts`, y = `Rushing Yards`)) +
+  geom_hline(yintercept = mean(team_data$`Rushing Yards`), lty = 2, color = "grey") +
+  geom_vline(xintercept = mean(team_data$`Rush Attempts`), lty = 2, color = "grey") +
+  geom_from_path(aes(path = logo), height = 0.05)
+
+# top player bar chart ----
+player_data %>%
+  slice_max(order_by = `Rushing TDs`, n = 10) %>%
+  ggplot(aes(x = reorder(player, -`Rushing TDs`), y = `Rushing TDs`)) +
+  geom_col(aes(fill = color, color = alt_color), alpha = 0.85) +
+  geom_from_path(aes(path = logo), height = 0.1) +
+  geom_from_path(aes(path = headshot_url, y = 1.5), height = 0.1) +
+  scale_fill_identity() +
+  scale_color_identity() +
+  labs(x = NULL) +
+  theme_minimal()
 
 # tooling around ----
-us_map <- maps::map(
-  database = "usa",
-  plot = FALSE,
-  fill = TRUE) %>%
-  st_as_sf()
+us_map <- map_data(map = "state") %>%
+  mutate(state = state.abb[match(region, tolower(state.name))]) %>%
+  select(long, lat, group, state)
 
-ggplot() +
-  geom_sf(data = us_map, color = "black") +
-  geom_cfb_logos(data = team_data, aes(x = longitude, y = latitude, team = school),
-                 height = 0.05, width = 0.05) +
+team_data %>%
+  summarize(avg_off = mean(`Total Yards`), .by = state) %>%
+  mutate(avg_off = floor((rank(avg_off) - 1) / (n() - 1) * 98) + 1) %>%
+  right_join(us_map, by = join_by(state == state)) %>%
+  ggplot(aes(long, lat, group = group)) +
+  geom_polygon(aes(fill = avg_off), color = "black") + 
+  coord_quickmap() +
   theme_void() +
-  coord_sf()
+  scale_fill_gradient2(low = "darkred", high = "darkgreen", midpoint = 50, mid = "white", na.value = "grey") +
+  theme(legend.position = "none")
